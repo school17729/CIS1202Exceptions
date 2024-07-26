50e0054f4709a374f4266d185b4159defca71bf2
school17729 <school17729@gmail.com>
2024-07-25 22:48:43 -0400

    Added driver function named test for use in main function

--- Source/Main.cpp
+++ Source/Main.cpp
@@ -9,8 +9,13 @@
 using namespace std;
 
 char character(char start, int offset);
+void test(char start, int offset);
 
 int main() {
+    test('a', 1);
+    test('a', -1);
+    test('Z', -1);
+    test('?', 5);
 }
 
 char character(char start, int offset) {
@@ -29,3 +34,19 @@ char character(char start, int offset) {
     return end;
 }
 
+void test(char start, int offset) {
+    cout << "Passing in " << start << " and " << offset << "." << endl;
+    char end;
+    bool succeeded;
+
+    succeeded = true;
+    try {
+        end = character(start, offset);
+    } catch (string exception) {
+        cout << "The exception was " << exception << endl;
+        succeeded = false;
+    }
+    if (succeeded) {
+        cout << "The result is " << end << endl;
+    }
+}
